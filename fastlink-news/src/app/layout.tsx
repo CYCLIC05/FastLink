c23e6f303f816e71426e5ff61a036b210a2927b9
Samuel <sammyturner.0ye@gmail.com>
2026-02-23 02:15:47 +0100

    favicon update

--- fastlink-news/src/app/layout.tsx
+++ fastlink-news/src/app/layout.tsx
@@ -11,6 +11,9 @@ const outfit = Outfit({ subsets: ["latin"], variable: "--font-inter" }); // Usin
 export const metadata: Metadata = {
   title: "FASTLINKNEWSAFRICA",
   description: "Leading news source for Africa and the world.",
+  icons: {
+    icon: "/Fastlink Africa_v3.jpg.jpeg",
+  },
 };
 
 export default function RootLayout({
